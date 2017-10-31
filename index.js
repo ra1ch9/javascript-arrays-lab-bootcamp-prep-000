d87eb55f6a7ceb393f96fcb73613e959edd62345
rachel schest <rschest@gmail.com>
2017-10-31 16:33:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = "I don't do much."
-var kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"];
+
 function destructivelyAppendKitten(name){
   push(name);
 }
